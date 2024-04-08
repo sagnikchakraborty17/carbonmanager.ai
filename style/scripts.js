fb5b07a34738f1e21b7294ceb2e9a652b38498f8
Team Hackminors <146724977+teamhackminors@users.noreply.github.com>
2024-04-08 13:45:37 +0530

    Update scripts.js

--- style/scripts.js
+++ style/scripts.js
@@ -3,7 +3,7 @@ var confirmation = confirm("The site will be plugging in its LLM (AI Chatbot) at
 
 // If confirmed, execute the code
 if (confirmation) {
-    window.open('javascript: var chatDiv = document.createElement(`div`); chatDiv.id = `botpressChat`; document.body.appendChild(chatDiv); var script1 = document.createElement(`script`); script1.src = `https://cdn.botpress.cloud/webchat/v1/inject.js`; document.body.appendChild(script1); var script2 = document.createElement(`script`); script2.src = `https://mediafiles.botpress.cloud/c80ccde7-2db0-4ca9-99b0-14c91448ee4e/webchat/config.js`; script2.defer = true; document.body.appendChild(script2); void(0);','_self');
+    window.open('javascript: var chatDiv = document.createElement(`div`); chatDiv.id = `botpressChat`; document.body.appendChild(chatDiv); var script1 = document.createElement(`script`); script1.src = `https://cdn.botpress.cloud/webchat/v1/inject.js`; script1.onload = function() { // Once inject.js is loaded, load config.js var script2 = document.createElement(`script`); script2.src = `https://mediafiles.botpress.cloud/c80ccde7-2db0-4ca9-99b0-14c91448ee4e/webchat/config.js`; script2.defer = true; document.body.appendChild(script2); }; document.body.appendChild(script1);void(0);','_self');
 } else {
     alert("You declined to plug in the AI Chatbot.");
 }
