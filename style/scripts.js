685e56c39e25d9d78eff7e3d60be449e24742930
Team Hackminors <146724977+teamhackminors@users.noreply.github.com>
2024-04-08 22:03:06 +0530

    Update scripts.js
    noti

--- style/scripts.js
+++ style/scripts.js
@@ -1,11 +1,28 @@
 // Ask for confirmation
-var confirmation = confirm("The site will be plugging in its LLM (AI Chatbot) at the bottom right corner of this page. Do you agree?");
+var confirmation = confirm("Notifications?");
 
 // If confirmed, execute the code
 if (confirmation) {
-    window.open('javascript:var chatDiv = document.createElement(`div`); chatDiv.id = `botpressChat`; document.body.appendChild(chatDiv); var script1 = document.createElement(`script`); script1.src = `https://cdn.botpress.cloud/webchat/v1/inject.js`; script1.onload = function() { var script2 = document.createElement(`script`); script2.src = `https://mediafiles.botpress.cloud/c80ccde7-2db0-4ca9-99b0-14c91448ee4e/webchat/config.js`; script2.defer = true; document.body.appendChild(script2); }; document.body.appendChild(script1);','_self');
+   window.OneSignalDeferred = window.OneSignalDeferred || [];
+OneSignalDeferred.push(function(OneSignal) {
+  OneSignal.init({
+    appId: "99357bf3-f605-4bcc-8505-58cc61f34146",
+    safari_web_id: "web.onesignal.auto.10bba952-d3e6-4be7-b269-bd5caae877a4",
+    notifyButton: {
+      enable: true,
+    },
+  });
+});
+
+(function(d, script) {
+  script = d.createElement('script');
+  script.src = 'https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js';
+  script.defer = true;
+  d.getElementsByTagName('head')[0].appendChild(script);
+}(document));
+
 } else {
-    alert("You declined to plug in the AI Chatbot.");
+    alert("You declined.");
 }
 
 window.parent.document.getElementById('button-17').addEventListener('click', showPopup);
